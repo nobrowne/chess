b6f685e1cf7be47ac091f17397d2129d6022f207
Nathan Browne <nobrowne@gmail.com>
2024-09-19 15:58:55 -0600

    feat: QueenMovementRules, pass tests

--- shared/src/main/java/chess/rules/QueenMovementRules.java
+++ shared/src/main/java/chess/rules/QueenMovementRules.java
@@ -4,12 +4,26 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class QueenMovementRules extends SharedMovementRules{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        // Orthogonal moves
+        calculateMoves(board, position, 1, 0, moves, true);
+        calculateMoves(board, position, -1, 0, moves, true);
+        calculateMoves(board, position, 0, -1, moves, true);
+        calculateMoves(board, position, 0, 1, moves, true);
+
+        calculateMoves(board, position, 1, -1, moves, true);
+        calculateMoves(board, position, 1, 1, moves, true);
+        calculateMoves(board, position, -1, -1, moves, true);
+        calculateMoves(board, position, -1, 1, moves, true);
+
+        return moves;
     }
 }
