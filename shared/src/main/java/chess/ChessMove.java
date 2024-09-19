e97eae892e7542faec7f3cc06db5167a8096ba87
Nathan Browne <nobrowne@gmail.com>
2024-09-18 20:02:33 -0600

    feat: functions, added equals, hashCode, and toString

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,22 +8,29 @@ package chess;
  */
 public class ChessMove {
 
+    private final ChessPosition startPosition;
+    private final ChessPosition endPosition;
+    private final ChessPiece.PieceType promotionPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPosition;
     }
 
     /**
@@ -33,6 +40,29 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ChessMove move = (ChessMove) obj;
+        return (startPosition.equals(move.startPosition) && endPosition.equals(move.endPosition)
+                && promotionPiece == move.promotionPiece);
+    }
+
+    @Override
+    public int hashCode() {
+        var promotionCode = (promotionPiece == null ?
+                9 : promotionPiece.ordinal());
+        return (71 * startPosition.hashCode()) + endPosition.hashCode() + promotionCode;
+    }
+
+    @Override
+    public String toString() {
+        var p = (promotionPiece == null ? "" : ":" + promotionPiece);
+        return String.format("%s:%s%s", startPosition.toString(), endPosition.toString(), p);
+    }
+
 }
