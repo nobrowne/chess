f46e4240d33b90e076f58b753122b4532585645f
Nathan Browne <nobrowne@gmail.com>
2024-09-19 15:51:26 -0600

    feat: Rook test pass

--- shared/src/main/java/chess/rules/RookMovementRules.java
+++ shared/src/main/java/chess/rules/RookMovementRules.java
@@ -4,14 +4,21 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
-public class RookMovementRules implements MovementRules {
+public class RookMovementRules extends SharedMovementRules {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        var pieceMoves = new HashSet<ChessMove>();
-        return null;
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        calculateMoves(board, position, 1, 0, moves, true);
+        calculateMoves(board, position, -1, 0, moves, true);
+        calculateMoves(board, position, 0, -1, moves, true);
+        calculateMoves(board, position, 0, 1, moves, true);
+
+        return moves;
     }
 }
