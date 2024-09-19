7e6246721bdd01c060cd270602c335f657ecb2f3
Nathan Browne <nobrowne@gmail.com>
2024-09-19 16:03:49 -0600

    feat: KingMovementRules, pass tests

--- shared/src/main/java/chess/rules/KingMovementRules.java
+++ shared/src/main/java/chess/rules/KingMovementRules.java
@@ -4,12 +4,26 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class KingMovementRules extends SharedMovementRules{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        // Orthogonal moves
+        calculateMoves(board, position, 1, 0, moves, false);
+        calculateMoves(board, position, -1, 0, moves, false);
+        calculateMoves(board, position, 0, -1, moves, false);
+        calculateMoves(board, position, 0, 1, moves, false);
+
+        // Diagonal moves
+        calculateMoves(board, position, 1, -1, moves, false);
+        calculateMoves(board, position, 1, 1, moves, false);
+        calculateMoves(board, position, -1, -1, moves, false);
+        calculateMoves(board, position, -1, 1, moves, false);
+
+        return moves;
     }
 }
