5e8ea07857ac06f0cfbc2ba09940b2a964dd13c0
Nathan Browne <nobrowne@gmail.com>
2024-09-30 20:48:52 -0600

    feat: constructor, add copy constructor

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -21,6 +21,11 @@ public class ChessPiece {
         this.type = type;
     }
 
+    public ChessPiece(ChessPiece other) {
+        this.pieceColor = other.pieceColor;
+        this.type = other.type;
+    }
+
     /**
      * The various different chess piece options
      */
