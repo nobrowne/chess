26df4497007883478958947e67140e2fffd80ed6
Nathan Browne <nobrowne@gmail.com>
2024-10-08 12:53:13 -0600

    Initial commit

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.AuthData;
+import model.UserData;
+
+public class UserService {
+    public AuthData register(UserData user) {
+        // TODO implement
+        return null;
+    }
+
+    public AuthData login(UserData user) {
+        // TODO implement
+        return null;
+    }
+
+    public void logout(UserData user) {
+        // TODO implement
+    }
+}
