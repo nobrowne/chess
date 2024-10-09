2411b6bd3b5a7ff6d1515781e9cec8398ae6e182
Nathan Browne <nobrowne@gmail.com>
2024-10-09 16:23:34 -0600

    feat: initial commit of class, add constructor and delete method

--- server/src/main/java/service/AdminService.java
+++ server/src/main/java/service/AdminService.java
@@ -1,7 +1,23 @@
 package service;
 
-public class AdminService {
-    public void clearApplication() {
+import dataaccess.auth.AuthDAO;
+import dataaccess.game.GameDAO;
+import dataaccess.user.UserDAO;
 
+public class AdminService {
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
+
+    public AdminService(AuthDAO authDAO, GameDAO gameDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+        this.userDAO = userDAO;
+    }
+
+    public void clear() {
+        authDAO.clear();
+        gameDAO.clear();
+        userDAO.clear();
     }
 }
