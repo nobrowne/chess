0d0f8855ac34542788c87c175e8cdc82aa394b1b
Nathan Browne <nobrowne@gmail.com>
2024-10-15 14:06:10 -0600

    feat: server, register register endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,6 +10,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", (request, response) -> "user");
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
