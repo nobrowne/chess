ac73aeda1365334697dc4f2d0e9dd2176ad30fef
Nathan Browne <nobrowne@gmail.com>
2024-10-17 13:54:56 -0600

    fix: Service, restore deleted code

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,5 +1,23 @@
 package service;
 
-public class Service {
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.UserData;
 
+public class Service {
+    private final DataAccess dataAccess;
+
+    public Service(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public UserData registerUser(UserData user) throws UsernameTakenException, DataAccessException {
+        if (dataAccess.getUser(user) != null) {
+            throw new UsernameTakenException("username already taken");
+        }
+
+        dataAccess.createUser(user);
+
+        return user;
+    }
 }
