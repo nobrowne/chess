9f6ba86ea5b34c9fff8f0709f68d05bd6cbd3a49
Nathan Browne <nobrowne@gmail.com>
2024-10-22 16:07:02 -0600

    setup: initial commit for starter code files

---
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -0,0 +1,23 @@
+package dataaccess;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DataAccessTests {
+
+    @BeforeAll
+    public static void init() {
+
+    }
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    public void firstTest() {
+
+    }
+}
