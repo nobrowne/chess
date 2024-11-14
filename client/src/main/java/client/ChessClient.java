4de783fd60ef10068186bf7ef5e4ba372ae65002
Nathan Browne <nobrowne@gmail.com>
2024-11-14 13:45:32 -0700

    feat: ChessClient, pass all positive and negative tests

---
+++ client/src/main/java/client/ChessClient.java
@@ -0,0 +1,61 @@
+package client;
+
+import exception.ResponseException;
+import java.util.Arrays;
+import model.AuthData;
+import model.UserData;
+import serverfacade.ServerFacade;
+
+public class ChessClient {
+  private final ServerFacade server;
+  private final String serverUrl;
+  private final State state = State.SIGNEDOUT;
+  private String username;
+  private String authToken;
+
+  public ChessClient(String serverUrl) {
+    server = new ServerFacade(serverUrl);
+    this.serverUrl = serverUrl;
+  }
+
+  public String eval(String input) {
+    try {
+      var tokens = input.toLowerCase().split(" ");
+      var command = (tokens.length > 0) ? tokens[0] : "help";
+      var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+      return switch (command) {
+        case "register" -> register(params);
+        case "login" -> login(params);
+        case "quit" -> "quit";
+        default -> help();
+      };
+    } catch (ResponseException ex) {
+      return ex.getMessage();
+    }
+  }
+
+  public String register(String... params) throws ResponseException {
+    if (params.length == 3) {
+      username = params[0];
+      String password = params[1];
+      String email = params[2];
+      UserData user = new UserData(username, password, email);
+
+      AuthData authData = server.register(user);
+      authToken = authData.authToken();
+
+      return String.format("You are registered as %s", username);
+    }
+    throw new ResponseException(
+        400, "username, password, or email are missing. Please provide all three");
+  }
+
+  public String login(String... params) {
+    return null;
+    // print the help statement for SIGNEDIN
+  }
+
+  public String help() {
+    return null;
+  }
+}
---
+++ client/src/test/java/client/ChessClientTests.java
@@ -0,0 +1,77 @@
+package client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import exception.ResponseException;
+import model.UserData;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import server.Server;
+import serverfacade.ServerFacade;
+
+public class ChessClientTests {
+  private static ChessClient chessClient;
+  private static ServerFacade serverFacade;
+  private static Server server;
+  private static UserData newUser;
+  private static UserData existingUser;
+
+  @BeforeAll
+  public static void init() {
+    server = new Server();
+    int port = server.run(0);
+
+    String serverUrl = "http://localhost:" + port;
+    chessClient = new ChessClient(serverUrl);
+    serverFacade = new ServerFacade(serverUrl);
+
+    newUser = new UserData("user1", "password", "email@email.com");
+    existingUser = new UserData("existingUser", "password", "email@email.com");
+  }
+
+  @AfterAll
+  public static void stopServer() {
+    server.stop();
+  }
+
+  @BeforeEach
+  public void setUp() throws ResponseException {
+    serverFacade.clearApplication();
+
+    chessClient.eval(
+        String.format(
+            "register %s %s %s",
+            existingUser.username(), existingUser.password(), existingUser.email()));
+  }
+
+  @Test
+  public void successfulRegistration() {
+    String registrationInfo =
+        String.format("register %s %s %s", newUser.username(), newUser.password(), newUser.email());
+    String registrationMessage = chessClient.eval(registrationInfo);
+
+    assertEquals("You are registered as " + newUser.username(), registrationMessage);
+  }
+
+  @Test
+  public void registeringWithMissingInfo() {
+    String registrationInfo =
+        String.format("register %s %s", newUser.username(), newUser.password());
+    String registrationMessage = chessClient.eval(registrationInfo);
+
+    assertEquals(
+        "username, password, or email are missing. Please provide all three", registrationMessage);
+  }
+
+  @Test
+  public void registeringWithTakenUsername() {
+    String registrationInfo =
+        String.format(
+            "register %s %s %s", existingUser.username(), newUser.password(), newUser.email());
+    String registrationMessage = chessClient.eval(registrationInfo);
+
+    assertEquals("error: username already taken", registrationMessage);
+  }
+}
